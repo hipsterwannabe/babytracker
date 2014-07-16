b189f6db807b2e54978593626fad2d00732a1957
Ashley Webb <ashleyleonawebb@gmail.com>
2014-07-16 14:43:32 -0500

    Deleted files.

--- babytracker/platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/NSData+Base64.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/danieljimenez/babytracker/platforms/ios/CordovaLib/Classes/NSData+Base64.m \
-  /Users/danieljimenez/babytracker/platforms/ios/CordovaLib/Classes/NSData+Base64.h
