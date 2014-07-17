9506b1cf5ba014d38ce00908cee131672144540e
Greg Vallejo <greg.vallejo@gmail.com>
2014-07-17 11:51:17 -0500

    completed nap blade logic

--- app/views/nap.blade.php
+++ app/views/nap.blade.php
@@ -6,33 +6,10 @@
 		<h1>Nap Form Mockup</h1><br>
 	<!-- laravel form helper here -->
 	<!-- button to start nap timer -->
-	<!-- buttong should change to STOP onclick -->
+	<!-- button should change to STOP onclick -->
     
 	    <div>
-	    	<script>
-	    		var startNap = null;
-		    	var stopNap = null;
-		        $('document').ready(function() {
-		        	// click event logs timestamp and changes button
-		        	if (startNap == null && stopNap == null) {
-		            $('#timer').click(function() {
-		               startNap = moment();
-		               $(this).removeClass("btn btn-success").addClass("btn btn-danger");
-		               $(this).text("STOP NAP");
-		               console.log(startNap).;
-		            });
-		        	}
-		        	// second click even logs stop time, changes text. button should prevent extra clicks
-		        	if ((typeof(startNap) != "undefined" && startNap !== null) && stopNap == null){
-		        	   $('#timer').click(function() {
-		               stopNap = moment();
-		               $(this).removeClass("btn btn-danger").addClass("btn btn-success");
-		               $(this).text("Wakey Wakey!");
-		               console.log(stopNap).;
-		            });
-		        	}
-		        });
-		    </script>
+	    	
 	    	<button type="button" class="btn btn-success" id="timer">START NAP</button>
 	    
 		</div>
@@ -41,9 +18,40 @@
 		{{ Form::label('notes', 'Notes') }}
 		<br>
 		{{ Form::textarea('notes', null, array('cols'=>'45', 'rows'=>'10', 'placeholder'=>'Write any notes here.', 'class'=>'wmd-input', 'id'=> 'wmd-input')) }}
+		<input id="begintime" type="hidden" name="startNap" value="">
+		<input id="endtime" type="hidden" name="stopNap" value="">
+
 		<div>
 	    	<button type="button" class="btn btn-primary">{{ Form::submit('Submit') }}</button>
 	    
 		</div>
 	</div>
+@stop
+
+@section('bottom-script')
+<script>
+	var startNap = null;
+	var stopNap = null;
+    $(document).ready(function() {
+    	// Click event logs timestamp and changes button
+    	// .click is being phased out
+    	// below will be a live event upon a click
+    	$(document).on('click', "#timer", function() {
+
+    		if (startNap == null && stopNap == null) {
+    			startNap = moment();
+               	$(this).removeClass("btn btn-success").addClass("btn btn-danger");
+               	$(this).text("STOP NAP");
+               	console.log(startNap);
+               	$("#begintime").val(startNap);
+    		} else if (startNap !== null && stopNap == null) {
+    			stopNap = moment();
+    			$(this).text("Wakey Wakey!");
+    			$(this).attr("disabled", "disabled");
+    			console.log(stopNap);
+    			$("#endtime").val(stopNap);
+    		}
+    	});
+    });	
+</script>
 @stop
