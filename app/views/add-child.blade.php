9cc427776527cfbd0f2b267b26d332e9d57e7ab6
Danny Jimenez <dannyalxndr@gmail.com>
2014-07-17 10:50:00 -0500

    added forms for the add child view

--- app/views/add-child.blade.php
+++ app/views/add-child.blade.php
@@ -4,18 +4,23 @@
 	
 	<div class="container">
 		<h1>Add Child</h1>
+		<hr>
 	</div>
 	<form action="POST">
-		<label>Name: </label>
-		<input type="text" placeholder="Name"><br>
-		<h2>Gender: </h2><br>
-        {{ Form::label('leak', 'M') }}
-        {{ Form::radio('leak', 'M') }}
-        <br>
-        {{ Form::label('leak', 'F') }}
-        {{ Form::radio('leak', 'F') }}
+		<input class="form-control" type="text" placeholder="Name"><br>
 
-		<label>Birth Date: </label>
-		<input type="text"><br>
+		<input class="form-control" type="text" placeholder="Date of birth"><br>
+		
+		<label>Gender: </label>
+		{{ Form::radio('gender', 'M') }}
+        {{ Form::label('gender', 'M') }}
+  
+        {{ Form::radio('gender', 'F') }}
+        {{ Form::label('gender', 'F') }}
+        <br>
+
+		
+
+		<button type="submit" class="btn btn-primary">Submit</button>
 	</form>
 @stop
