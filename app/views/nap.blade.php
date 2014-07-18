db51ff881bde94cc6ac3f7556b6e76ff249adf94
Greg Vallejo <greg.vallejo@gmail.com>
2014-07-18 13:37:37 -0500

    added timer which starts and stops on click

--- app/views/nap.blade.php
+++ app/views/nap.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('topscript')
-<link rel="stylesheet" type="text/css" href="/assets/FLipClock-master/compiled/flipclock.css">
+<link rel="stylesheet" type="text/css" href="/assets/FlipClock-master/compiled/flipclock.css">
 @stop
 
 @section('content')
@@ -9,17 +9,18 @@
 		<h1>Nap Form Mockup</h1><br>
 	<!-- laravel form helper here -->
 	<!-- button to start nap timer -->
-	<!-- buttong should change to STOP onclick -->
+	<!-- button should change to STOP onclick -->
     
 	    <div>
 	    	<div class="row">
 	    		<div class="col-md-2">
-	    		<button type="button" class="btn btn-success" id="timer">START NAP</button>
+	    		   <button type="button" class="btn btn-success" id="timer">START NAP</button>
 	    		</div>
-	    		<div class="col-md-2" id="napTimer"></div>
+          <div></div>
+	    		<div class="col-md-10" id="napTimer"></div>
 			</div>
 	    <br>
-		
+		{{-- Form::open --}}
 		{{ Form::label('notes', 'Notes') }}
 		<br>
 		{{ Form::textarea('notes', null, array('cols'=>'45', 'rows'=>'10', 'placeholder'=>'Write any notes here.', 'class'=>'wmd-input', 'id'=> 'wmd-input')) }}
@@ -27,7 +28,7 @@
 		<input id="endtime" type="hidden" name="stopNap" value="">
 		<div>
 	    	<button type="button" class="btn btn-primary">{{ Form::submit('Submit') }}</button>
-	    
+	  {{ Form::close() }}  
 		</div>
 	</div>
 @stop
@@ -35,7 +36,7 @@
 @section('bottomscript')
 <script type="text/javascript" src="js/jquery.plugin.js"></script> 
 <script type="text/javascript" src="js/jquery.countdown.js"></script>
-<script type="text/javascript" src="/assets/FLipClock-master/compiled/flipcock.js"></script>
+<script type="text/javascript" src="/assets/FlipClock-master/compiled/flipclock.js"></script>
 
 <script>
 	var startNap = null;
@@ -52,20 +53,24 @@
          	$(this).text("STOP NAP");
          	console.log(startNap);
           $("#begintime").val(startNap);
-         	//timer to go here, using flipclock
-         	
+         	  //timer to go here, using flipclock
+         	  flipClock = $('#napTimer').FlipClock({ 
+          });
 
-         	});
-    		} else if (startNap !== null && stopNap == null) {
-    			stopNap = moment();
+        } else if (startNap !== null && stopNap == null) {
+    			// logs time of stopNap, disables button
+          stopNap = moment();
     			$(this).text("Wakey Wakey!");
     			$(this).attr("disabled", "disabled");
     			console.log(stopNap);
     			$("#endtime").val(stopNap);
+          //stop the flipclock timer
+          flipClock.stop();
     		}
-    	});
-    });	
+      });
+    });
 	
+	  
 </script>
 
 @stop
