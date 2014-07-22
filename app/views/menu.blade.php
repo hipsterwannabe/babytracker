cf39a9604dde16622cb0c32551d72fc4a38d6a87
Ashley Webb <ashleyleonawebb@gmail.com>
2014-07-21 23:05:42 -0500

    Fixing paths, styling.

--- app/views/menu.blade.php
+++ app/views/menu.blade.php
@@ -9,12 +9,12 @@
         <div class="col-lg-6">
             <!-- Baby picture and info -->
             <div class="media pull-left">
-                <a href="/baby-stats"><img class="media-object img-circle img-responsive" src="{{{ $baby->img_path }}}"></a>
+                <a href="/baby-stats"><img class="media-object img-responsive" src="{{{ $baby->img_path }}}"></a>
             </div>
 
             <div class="col-lg-6">
-                <h2 class="featurette-heading"> {{ $baby->name }}</h2>
-                <p class="text-muted">{{ $baby->birth_date }}</p>
+                <h2> {{ $baby->name }} </h2>
+                <p>{{ $baby->birth_date }}</p>
                 <!-- Select event to log -->
                 <div class="btn-group-vertical">
                     <a href="{{{ action('EventController@showDiaper', $baby->id) }}}" class="btn btn-block btn-primary" role="button">Diaper</a>
