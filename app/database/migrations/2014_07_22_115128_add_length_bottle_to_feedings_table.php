118170080d90d1778676caf861cbcc6bfd64ca3a
Greg Vallejo <greg.vallejo@gmail.com>
2014-07-22 11:55:00 -0500

    New migration for feedings table

---
+++ app/database/migrations/2014_07_22_115128_add_length_bottle_to_feedings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLengthBottleToFeedingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('feedings', function(Blueprint $table)
+		{
+			$table->time('bottle_length');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('feedings', function(Blueprint $table)
+		{
+			$table->dropColumn('bottle_length');
+		});
+	}
+
+}
