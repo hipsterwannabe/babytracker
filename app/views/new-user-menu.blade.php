692cae1b3d04ca71ccd766be759d79451e48a12a
Danny Jimenez <dannyalxndr@gmail.com>
2014-07-22 11:23:37 -0500

    working on sign up process.

--- app/views/new-user-menu.blade.php
+++ app/views/new-user-menu.blade.php
@@ -16,11 +16,8 @@
 <div class="container">
 
     <div class="col-lg-offset-4 col-lg-4">
-        <h1>Select Child</h1>
+        <h1>Welcome!</h1>
         <div class="btn-group-vertical btn-block" id="btnOptions">
-            @foreach ( Auth::user()->babies as $baby)
-                <a href="/menu/{{$baby->id}}" class="btn btn-primary btn-lg" role="button">{{ $baby->name }}</a>
-            @endforeach
             <a href="{{{ action('UserController@showBaby', Auth::id()) }}}" class="btn btn-info btn-lg" role="button">Add Baby</a>
         </div>
     </div>
