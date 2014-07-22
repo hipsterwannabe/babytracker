825fcbe623adc86f9b5a066f970018eb412418a0
Danny Jimenez <dannyalxndr@gmail.com>
2014-07-22 14:59:14 -0500

    sign up feature FINALLY works without errors!!

--- app/views/new-user-menu.blade.php
+++ app/views/new-user-menu.blade.php
@@ -18,7 +18,7 @@
     <div class="col-lg-offset-4 col-lg-4">
         <h1>Welcome!</h1>
         <div class="btn-group-vertical btn-block" id="btnOptions">
-            <a href="{{{ action('UserController@showCreateBaby) }}}" class="btn btn-info btn-lg" role="button">Add Baby</a>
+            <a href="{{{ action('UserController@showCreateBaby') }}}" class="btn btn-info btn-lg" role="button">Add Baby</a>
         </div>
     </div>
 
