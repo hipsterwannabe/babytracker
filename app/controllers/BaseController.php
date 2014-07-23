f67cddc9e34ffd3b79e6bde3ccdb6f0d2a01f05a
Ashley Webb <ashleyleonawebb@gmail.com>
2014-07-23 10:37:26 -0500

    Corrected formatting.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,24 +2,24 @@
 
 class BaseController extends Controller {
 
-	// public function __construct()
+    // public function __construct()
  //    {
-    	
+
  //        // run auth filter before all methods on this controller except index and show
  //        $this->beforeFilter('auth.basic', array('except' => array('login')));
 
  //    }
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function setupLayout()
+    {
+        if ( ! is_null($this->layout))
+        {
+            $this->layout = View::make($this->layout);
+        }
+    }
 
 }
